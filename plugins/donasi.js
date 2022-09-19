8cdee53a1bfc7dbbcd65eceb1bb2dc0537bfc334
Ryuuzxy <109670909+Ryuuzxy@users.noreply.github.com>
2022-09-19 12:45:12 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+┌─「 Donasi • Pulsa 」
+│ • *Indosat:* [${global.ppulsa}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Ovo:* [${global.povo}]
+❏────
+`.trim()
+  m.reply(text)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
