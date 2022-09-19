86c6f6154fee488a07c7351db1747bb9d8c0a7b4
Ryuuzxy <109670909+Ryuuzxy@users.noreply.github.com>
2022-09-19 12:46:13 +0700

    Add files via upload

---
+++ plugins/hello.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp3/little-girl-sayingtalking-hello-sound-effectcute-sound-effectsadorable.opus')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://youtube.com/channel/UCBYk6B1tkZ233ElCqImkC7Q",
+    mediaType: 2,
+    description: "https://youtube.com/channel/UCBYk6B1tkZ233ElCqImkC7Q", 
+    title: 'タRyuuzxyあ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.customPrefix = /^(hallo|hello|alo|allo)$/i
+handler.command = new RegExp
+
+export default handler
